ddb21063354561c48d751ef00f9aa23efc87d043
zhangshyue <zhangshiyuez@gmail.com>
2020-10-23 22:19:05 -0500

    create unity project

---
+++ Assets/Scripts/CameraMotor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMotor : MonoBehaviour
+{
+    private Transform lookAt;
+    private Vector3 startOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lookAt = GameObject.FindGameObjectWithTag("Player").transform;
+        startOffset = transform.position - lookAt.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = lookAt.position + startOffset;
+    }
+}
