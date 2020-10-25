a7f39e9c05a44789243c737507240af836825ebf
zhangshyue <zhangshiyuez@gmail.com>
2020-10-25 18:29:53 -0500

    change cube to T-Rex

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -8,26 +8,41 @@ public class PlayerMotor : MonoBehaviour
     private float speed = 3.0f;
     private Vector3 moveVector;
 
+    // how high we want to jump
+    public float jumpForce = 100;
+
+    public BoxCollider col;
+    public Rigidbody rb;
+
     void Start () {
         controller = GetComponent<CharacterController> ();
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<BoxCollider>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        moveVector = Vector3.zero;
+    moveVector = Vector3.zero;
 
-        // left and right
-        moveVector.x = Input.GetAxisRaw("Horizontal") * 4.0f;
+    // left and right
+    moveVector.x = Input.GetAxisRaw("Horizontal") * 4.0f;
 
-        // forward
-        moveVector.z = speed;
+    // forward
+    moveVector.z = speed;
 
-        controller.Move(moveVector * speed * Time.deltaTime);
+    // jumping
+    if (controller.isGrounded && Input.GetKeyDown(KeyCode.Space)){
+      moveVector.y += 10f * speed;
+    }else{
+      moveVector.y -= 20f * speed * Time.deltaTime;
+    }
+    
+    controller.Move(moveVector * speed * Time.deltaTime);
 	}
 
-    // Update speed according to difficulty level
-    public void SetSpeed(int modifier)
-    {
-        speed = 3.0f + modifier/4.0f;
-    }
+  // Update speed according to difficulty level
+  public void SetSpeed(int modifier)
+  {
+      speed = 3.0f + modifier/4.0f;
+  }
 }
